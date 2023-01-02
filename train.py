0df5a412081a6727f0e9cf859c5b129f6aa475dd
siryuon <siryuon@gmail.com>
2023-01-02 16:28:50 +0000

    feat: apply kfold code to train.py

--- train.py
+++ train.py
@@ -3,8 +3,9 @@ import os
 import sys
 from typing import NoReturn
 
+import pandas as pd
+
 from arguments import DataTrainingArguments, ModelArguments
-from datasets import DatasetDict, load_from_disk, load_metric
 from trainer_qa import QuestionAnsweringTrainer
 from transformers import (
     AutoConfig,
@@ -18,6 +19,19 @@ from transformers import (
 )
 from utils_qa import check_no_error, postprocess_qa_predictions
 
+from datasets import (
+    load_metric,
+    load_from_disk,
+    Dataset,
+    DatasetDict,
+    Features,
+    Sequence,
+    Value,
+    concatenate_datasets,
+)
+
+import wandb
+
 logger = logging.getLogger(__name__)
 
 
@@ -26,9 +40,9 @@ def main():
     # --help flag 를 실행시켜서 확인할 수 도 있습니다.
 
     parser = HfArgumentParser(
-        (ModelArguments, DataTrainingArguments, TrainingArguments)
+        (ModelArguments, DataTrainingArguments)
     )
-    model_args, data_args, training_args = parser.parse_args_into_dataclasses()
+    model_args, data_args = parser.parse_args_into_dataclasses()
     print(model_args.model_name_or_path)
 
     fold1 = pd.read_csv('./fold1_test.csv')
@@ -44,67 +58,111 @@ def main():
     fold5["answers"] = fold5["answers"].apply(eval)
 
     folds = [fold1, fold2, fold3, fold4, fold5]
-    # [참고] argument를 manual하게 수정하고 싶은 경우에 아래와 같은 방식을 사용할 수 있습니다
-    # training_args.per_device_train_batch_size = 4
-    # print(training_args.per_device_train_batch_size)
 
-    print(f"model is from {model_args.model_name_or_path}")
-    print(f"data is from {data_args.dataset_name}")
+    for fold in range(1, 6):
+        training_args = TrainingArguments(
+            do_train=True,
+            output_dir="./models/train_dataset_5_fold" + str(fold),
+            overwrite_output_dir=True,
+            evaluation_strategy="steps",
+            per_device_train_batch_size=16,
+            per_device_eval_batch_size=16,
+            gradient_accumulation_steps=2,
+            learning_rate=1e-5,
+            num_train_epochs=8,
+            warmup_ratio=0.1,
+            logging_strategy="steps",
+            logging_steps=100,
+            save_strategy="steps",
+            save_steps=300,
+            save_total_limit=1,
+            seed=42,
+            eval_steps=300,
+            metric_for_best_model="exact_match",
+            load_best_model_at_end=True,
+        )
 
-    # logging 설정
-    logging.basicConfig(
-        format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
-        datefmt="%m/%d/%Y %H:%M:%S",
-        handlers=[logging.StreamHandler(sys.stdout)],
-    )
+        print(f"model is from {model_args.model_name_or_path}")
+        print(f"data is from {data_args.dataset_name}")
 
-    # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
-    logger.info("Training/evaluation parameters %s", training_args)
+        # logging 설정
+        logging.basicConfig(
+            format="%(asctime)s - %(levelname)s - %(name)s -    %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+            handlers=[logging.StreamHandler(sys.stdout)],
+        )
 
-    # 모델을 초기화하기 전에 난수를 고정합니다.
-    set_seed(training_args.seed)
+        # verbosity 설정 : Transformers logger의 정보로 사용합니다 (on main process only)
+        logger.info("Training/evaluation parameters %s", training_args)
+        df = pd.concat([folds[i] for i in range(5) if i + 1 != fold], ignore_index=True)
+        # 모델을 초기화하기 전에 난수를 고정합니다.
+        set_seed(training_args.seed)
 
-    datasets = load_from_disk(data_args.dataset_name)
-    print(datasets)
-    if model_args.config_name is not None:
-        print("modelname!:  ",model_args.config_name)
-    else: 
-        print("model!!",model_args.model_name_or_path)
-    # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
-    # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
-    config = AutoConfig.from_pretrained(
-        model_args.config_name
-        if model_args.config_name is not None
-        else model_args.model_name_or_path,
-    )
+        f = Features(
+            {
+                "answers": Sequence(
+                    feature={
+                        "text": Value(dtype="string", id=None),
+                        "answer_start": Value(dtype="int32", id=None),
+                    },
+                    length=-1,
+                    id=None,
+                ),
+                "context": Value(dtype="string", id=None),
+                "id": Value(dtype="string", id=None),
+                "question": Value(dtype="string", id=None),
+            }
+        )
+        datasets = DatasetDict(
+            {
+                "train": Dataset.from_pandas(df),
+                "validation": Dataset.from_pandas(folds[fold - 1]),
+            }
+        )
 
-    tokenizer = AutoTokenizer.from_pretrained(
-        model_args.tokenizer_name
-        if model_args.tokenizer_name is not None
-        else model_args.model_name_or_path,
-        # 'use_fast' argument를 True로 설정할 경우 rust로 구현된 tokenizer를 사용할 수 있습니다.
-        # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며,
-        # rust version이 비교적 속도가 빠릅니다.
-        use_fast=True,
-    )
-    model = AutoModelForQuestionAnswering.from_pretrained(
-        model_args.model_name_or_path,
-        from_tf=bool(".ckpt" in model_args.model_name_or_path),
-        config=config,
-    )
+        print(datasets)
+        # AutoConfig를 이용하여 pretrained model 과 tokenizer를 불러옵니다.
+        # argument로 원하는 모델 이름을 설정하면 옵션을 바꿀 수 있습니다.
+        config = AutoConfig.from_pretrained(
+            model_args.config_name
+            if model_args.config_name is not None
+            else model_args.model_name_or_path,
+        )
 
-    print(
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_args.tokenizer_name
+            if model_args.tokenizer_name is not None
+            else model_args.model_name_or_path,
+            # 'use_fast' argument를 True로 설정할 경우 rust로 구현된 tokenizer를 사용할 수 있습니다.
+            # False로 설정할 경우 python으로 구현된 tokenizer를 사용할 수 있으며,
+            # rust version이 비교적 속도가 빠릅니다.
+            use_fast=True,
+        )
+        model = AutoModelForQuestionAnswering.from_pretrained(
+            model_args.model_name_or_path,
+            from_tf=bool(".ckpt" in model_args.model_name_or_path),
+            config=config,
+        )
+
+        print(
         
-        type(training_args),
-        type(model_args),
-        type(datasets),
-        type(tokenizer),
-        type(model),
-    )
+            type(training_args),
+            type(model_args),
+            type(datasets),
+            type(tokenizer),
+            type(model),
+        )
 
-    # do_train mrc model 혹은 do_eval mrc model
-    if training_args.do_train or training_args.do_eval:
-        run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
+        # do_train mrc model 혹은 do_eval mrc model
+        if training_args.do_train or training_args.do_eval:
+            run = wandb.init(
+                project="kfold_exp",
+                entity="nlp-08-mrc",
+                name="fold" + str(fold),
+                group="5_fold",
+            )
+            run_mrc(data_args, training_args, model_args, datasets, tokenizer, model)
+            run.finish()
 
 
 def run_mrc(
